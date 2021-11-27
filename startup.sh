f66eab471ef5619baf64026006be72b3cc46dbe7
Ramprakash <prakashram1996@gmail.com>
2021-11-27 01:33:10 -0500

    auto yes for package installation

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 apt-get update -y
-apt-get install libgtk2.0-dev
-apt-get install libjpeg-dev zlib1g-dev
+apt-get -y install libgtk2.0-dev
+apt-get -y install libjpeg-dev zlib1g-dev
 gunicorn --bind=0.0.0.0 --timeout 600 --chdir Front-end app:app
