d65f3b1b6f2b2f72e5bea451c545f088e04c0b38
Ramprakash <prakashram1996@gmail.com>
2021-11-27 03:06:09 -0500

    start

--- startup.sh
+++ startup.sh
@@ -1,1 +1,4 @@
-gunicorn --bind=0.0.0.0:8000 --timeout 600 --chdir Front-end app:app
+apt-get update -y
+apt-get -y install libgtk2.0-dev
+apt-get -y install libjpeg-dev zlib1g-dev
+gunicorn --bind=0.0.0.0 --timeout 600 --chdir Front-end app:app
